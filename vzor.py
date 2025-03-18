f712a1cbf8e6de8cfc6c2638ebd0faaebe905c49
unknown <antonin.burianek@infis.cz>
2025-03-18 09:28:38 +0100

    vzor

---
+++ vzor.py
@@ -0,0 +1,16 @@
+rok = 2025
+text = "Veselé Velikonoce"
+cislo = 51054601
+
+print(f"Ahoj, {test} {rok}")
+
+print(f"{cislo:,}")
+
+print(f"{text:.6}")
+
+print(f"{cislo:.3f}")
+
+hodnota = 155
+print(f"{hodnota:b}")
+print(f"{hodnota:X}")
+
